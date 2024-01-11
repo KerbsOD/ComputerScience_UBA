55d0e87c9801a5b17921ff10e3380cf3b562daef
KerbsOD <kerbsod@gmail.com>
2024-01-11 06:18:44 -0300

    Ejercicio 8 y auxiliares

--- Algo-3/Practicas/Practica1-Tecnicas/Ejercicio8.go
+++ Algo-3/Practicas/Practica1-Tecnicas/Ejercicio8.go
@@ -19,6 +19,12 @@
 
 package main
 
+import (
+	"math"
+)
+
+const inf int = math.MaxInt32 >> 8
+
 func cortarVara(i int, j int, C []int, M [][]int) int {
 	if j <= i {
 		M[i][j] = 0
@@ -51,17 +57,7 @@ func cortarVara(i int, j int, C []int, M [][]int) int {
 	Solo podemos entre 4 y 10.
 	Entonces si j <= i, devolvemos 0. No podemos calcular el corte.
 
-	Sabiendo esto, en el algoritmo Bottom-Up llenamos todos los casilleros donde j <= i con 0 y el resto con infinito.
-
 	Ejemplo con l = 4 y C = {2, 3} (son 5 posiciones porque estamos 0-indexados)
-
-	  __0____1_____2_____3_____4___
-	0 | 0 | inf | inf | inf | inf |
-	1 | 0 | 0   | inf | inf | inf |
-	2 | 0 | 0   | 0   | inf | inf |
-	3 | 0 | 0   | 0   | 0   | inf |
-	4 | 0 | 0   | 0   | 0   | 0   |
-	  -----------------------------
 
 	Luego por definicion del algoritmo top down sabemos que:
 
@@ -84,29 +80,24 @@ func cortarVara(i int, j int, C []int, M [][]int) int {
 	j = 3, i = 2 -> M[2][3] = 0
 	j = 3, i = 1 -> M[1][3] = 0
 	j = 3, i = 0 -> M[0][3] = min(M[0][3], M[0][2]+M[2][3]) + 3-0. Como existe un c entre 0 y 3 (c=2), se hace la cuenta y gracias a que calculamos
-	 														 las "dependencias" de M[0][3] vamos a poder hacer la cuenta :)
+	 														 las "dependencias" de M[0][3], vamos a poder hacer la cuenta :)
 
 	  __0____1_____2_____3_____4___
-	0 | 0 | 0   | 0   | 3   | inf |
-	1 | 0 | 0   | 0   | 0   | inf |
-	2 | 0 | 0   | 0   | 0   | inf |
-	3 | 0 | 0   | 0   | 0   | inf |
-	4 | 0 | 0   | 0   | 0   | 0   |
+	0 | 0   | 0     | 0     | 3   | inf |
+	1 | inf | 0     | 0     | 0   | inf |
+	2 | inf | inf   | 0     | 0   | inf |
+	3 | inf | inf   | inf   | 0   | inf |
+	4 | inf | inf   | inf   | inf | 0   |
 	  -----------------------------
+
+	Complejidad: (l-1) + (l-2) + (l-3) ... 0 = O(l^2)
+
+	Me esta costando un poco calulcarla (vuelvo al cbc :p) pero para cada x entre 0 y l tenemos x-1 iteraciones. O sea es (l-1) + (l-2) + (l-3)..
+	hasta 0. Podemos acotarlo por arriba y decir que el peor caso es O(l^2).
 
 */
 
 func cortarVaraBU(l int, C []int, M [][]int) int {
-	for i := 0; i <= l; i++ {
-		for j := 0; j <= l; j++ {
-			if j <= i {
-				M[i][j] = 0
-			} else {
-				M[i][j] = inf
-			}
-		}
-	}
-
 	for j := 0; j <= l; j++ {
 		for i := j - 1; i >= 0; i-- {
 			existeAlgunCorte := false
@@ -127,3 +118,27 @@ func cortarVaraBU(l int, C []int, M [][]int) int {
 
 	return M[0][l]
 }
+
+// func main() {
+// 	C := []int{2, 4, 7, 9, 13, 18, 34, 36, 56, 78, 82, 99}
+// 	l := 100
+// 	M := make([][]int, l+1)
+// 	for i := 0; i <= l; i++ {
+// 		M[i] = make([]int, l+1)
+// 		for j := 0; j <= l; j++ {
+// 			M[i][j] = -1
+// 		}
+// 	}
+
+// 	timeStart := time.Now()
+// 	resultado := cortarVara(0, l, C, M)
+// 	timeStop := time.Since(timeStart)
+// 	fmt.Printf("CortarVara Top-Down \nResultado: %v \nTiempo: %v \n", resultado, timeStop)
+
+// 	fmt.Println()
+
+// 	timeStart = time.Now()
+// 	resultado = cortarVaraBU(l, C, M)
+// 	timeStop = time.Since(timeStart)
+// 	fmt.Printf("CortarVara Bottom-Up \nResultado: %v \nTiempo: %v \n", resultado, timeStop)
+// }
