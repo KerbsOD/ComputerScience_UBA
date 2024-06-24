326d2fd3bf919e70bb47043c5f127b21531b50d3
KerbsOD <kerbsod@gmail.com>
2024-06-24 09:56:48 -0300

    Ej 23 nodo y arista

---
+++ PLP/Guias/Guia8-Logica/ejercicio23.pl
@@ -0,0 +1,10 @@
+%%esNodo(+G, ?X)
+esNodo(grafo([V | _],_), V).
+esNodo(grafo([_ | VS],_), X) :- esNodo(grafo(VS,_),X).
+
+%%esArista(+G,?X,?Y)
+esArista(grafo(_, [arista(E1,E2) | _]), X, Y) :- var(X), var(Y), X = E1, Y = E2.
+esArista(grafo(_, [arista(E1,E2) | _]), X, Y) :- nonvar(X), nonvar(Y), X is E1, Y is E2.
+esArista(grafo(_, [arista(E1,E2) | _]), Y, X) :- nonvar(X), nonvar(Y), X is E1, Y is E2.
+esArista(grafo(_, [_ | ES]), X, Y) :- esArista(grafo(_,ES),X,Y).
+
