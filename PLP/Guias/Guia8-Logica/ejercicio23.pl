63810b52c92a1a46ed77f08db1125267b8a564ab
KerbsOD <kerbsod@gmail.com>
2024-06-24 10:07:51 -0300

    Chequeando que los extremos de la arista sean nodos tambien

--- PLP/Guias/Guia8-Logica/ejercicio23.pl
+++ PLP/Guias/Guia8-Logica/ejercicio23.pl
@@ -6,5 +6,5 @@ esNodo(grafo([_ | VS],_), X) :- esNodo(grafo(VS,_),X).
 esArista(grafo(_, [arista(E1,E2) | _]), X, Y) :- var(X), var(Y), X = E1, Y = E2.
 esArista(grafo(_, [arista(E1,E2) | _]), X, Y) :- nonvar(X), nonvar(Y), X is E1, Y is E2.
 esArista(grafo(_, [arista(E1,E2) | _]), Y, X) :- nonvar(X), nonvar(Y), X is E1, Y is E2.
-esArista(grafo(_, [_ | ES]), X, Y) :- esArista(grafo(_,ES),X,Y).
+esArista(grafo(V, [_ | ES]), X, Y) :- esNodo(grafo(V, _), X), esNodo(grafo(V, _), Y), esArista(grafo(V,ES),X,Y).
 
