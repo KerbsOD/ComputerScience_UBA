1485b332120e05bbc9b8047eefb7423e5571b210
kerbsod <kerbsod@gmail.com>
2024-08-19 14:31:26 -0300

    C es pain

--- SistemasOperativos/Guia0-Comandos/Ejercicio11.c
+++ SistemasOperativos/Guia0-Comandos/Ejercicio11.c
@@ -4,12 +4,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    char *cuit = argv[1];
+int validCuit(char* cuit, char* anotherCuit) {
+    if (strcmp(cuit, anotherCuit) == 0) {
+        printf("El CUIT ingresado es valido");
+    } else {
+        printf("El CUIT ingresado no es válido. Su información corregida es %s", anotherCuit);
+    }
+}
 
+char* calcularCuit(char* cuit) {
     int scalars[] = {5, 4, 3, 2, 7, 6, 5, 4, 3, 2};
     int verifierDigit = 0;
+    char correctCuit[11];
+
+    strcpy(correctCuit, cuit);
+
     for(int i = 0; i < 10; i++) {
         int number = cuit[i] - '0';
         verifierDigit += number * scalars[i];
@@ -19,11 +30,12 @@ int main(int argc, char *argv[]) {
     verifierDigit = 11 - verifierDigit;
     char verifierDigitToChar = '0' + verifierDigit;
 
-    if (cuit[10] == verifierDigitToChar) {
-        printf("El CUIT ingresado es valido");
-    } else {
-        cuit[10] = verifierDigitToChar;
-        printf("El CUIT ingresado no es válido. Su información corregida es %s", cuit);
-    }
+    correctCuit[10] = verifierDigitToChar;
 
+    return correctCuit;
 }
+
+//int main(int argc, char *argv[]) {
+//    char *cuit = argv[1];
+//    validCuit(cuit, calcularCuit(cuit));
+//}
